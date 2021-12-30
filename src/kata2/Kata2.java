04116dd0053fc9fe0800a83d8420926f5deefb5a
vj_62 <vj_62@DESKTOP-ES4EHF5>
2021-12-30 13:05:49 +0000

    Tercera version Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -28,16 +28,9 @@ public class Kata2 {
     public static void main(String[] args) {
         
         int[] data = {2,3,7,2,4,5,1,22,44,67,5,5,3,22,8};
-        
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        
-        
-        for (int i : data) {
-            histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1 : 1);
-            System.out.println(i);
-        }
-        
-        Kata2.showMe(histogram);
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        Kata2.showMe(histogr);
     }
 
 }
